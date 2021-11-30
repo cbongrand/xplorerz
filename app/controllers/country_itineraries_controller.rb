1156d0f898e04932ac66e1fcaf9d038d5af42f2d
Maryan Berhe <maryanberhe@gmail.com>
2021-11-30 13:12:28 -0600

    created the controllers/not coded

---
+++ app/controllers/country_itineraries_controller.rb
@@ -0,0 +1,2 @@
+class CountryItinerariesController < ApplicationController
+end
---
+++ test/controllers/itinerary_tags_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ItineraryTagsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
