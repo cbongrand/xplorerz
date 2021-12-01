4f0f44173456c083b22e57f9b1d09116a55b49f8
Maryan Berhe <maryanberhe@gmail.com>
2021-12-01 14:35:25 -0600

    wizard idea was ditched, updated itineraries controller and routes

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -2,11 +2,24 @@ class ItinerariesController < ApplicationController
   def index
     @itineraries = Itinerary.all
   end
+
   def new
-    @itinerary = Itinerary.new
+    @itinerary = Itinerary.new(itinerary_params)
   end
 
   def create
-    @itinerary = Itinerary.new
+    @itinerary = Itinerary.new(itinerary_params)
+    @itinerary.user = current_user
+    if @itinerary.save
+      redirect_to itinerary_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:title, :description)
   end
 end
