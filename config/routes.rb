53f77d0846a9b5e893758e0cd72aaa876f1b2b57
Sara Kahankova <cookietar91@gmail.com>
2021-12-01 14:45:07 -0600

    new and create booking actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :itineraries, only: %i[create new show index] do
-    resources :bookings, only: [:new]
+    resources :bookings, only: %i[new create]
   end
 
   get "/profile", to: "pages#profile"
   get "/credits", to: "pages#credits"
   patch "/credits", to: "pages#purchase"
-  get "/confirmation/booking_id", to: "bookings#confirm"
+  get "/bookings/:id/confirm", to: "bookings#confirm", as: 'booking_confirmation'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
