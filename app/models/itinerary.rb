5250294e2a6e52be3d3c78b62cb6fe297d510ba6
Raymond Klauwer <klauwerraymond@gmail.com>
2021-12-07 16:46:30 -0600

    fixed card-image for index-page

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -10,11 +10,10 @@ class Itinerary < ApplicationRecord
   acts_as_likeable
   validates :title, presence: true
   validates :description, presence: true
+  has_one_attached :photo
 
   def booked?(current_user)
     current_user_bookings = current_user.booked_itineraries
     current_user_bookings.any? { |user_booking| user_booking.id == id }
   end
-  
-  has_one_attached :photo
 end
