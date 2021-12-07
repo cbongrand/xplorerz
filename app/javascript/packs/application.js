0c6f496b30e3489e1e9f4dc110c8f72ed1cef214
Cesar Bongrand <mrbongrand@gmail.com>
2021-12-07 12:07:53 -0600

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,9 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
