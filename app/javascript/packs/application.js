22d8bce9b640e51aa258a9a07db73fc0f090a385
Cesar Bongrand <mrbongrand@gmail.com>
2021-12-07 16:10:02 -0600

    ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
