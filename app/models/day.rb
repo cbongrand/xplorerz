c2517c73caae3ee1bb134ed5b0ef12c14da1cce1
Cesar Bongrand <mrbongrand@gmail.com>
2021-12-07 12:13:05 -0600

    ok

--- app/models/day.rb
+++ app/models/day.rb
@@ -7,5 +7,5 @@ class Day < ApplicationRecord
   # validates :order, presence: true
   validates :extra_info, presence: true
   geocoded_by :city
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_city?
 end
