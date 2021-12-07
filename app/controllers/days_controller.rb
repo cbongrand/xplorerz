e5b04d2e269240fe5c6ab9ebcabe1f0313378dc3
Raymond Klauwer <klauwerraymond@gmail.com>
2021-12-07 12:16:00 -0600

    updated showpage itinerary

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,5 +1,5 @@
 class DaysController < ApplicationController
-  before_action :pluralize
+  # before_action :pluralize
 
   def new
     @itinerary = Itinerary.find(params[:itinerary_id])
@@ -41,15 +41,15 @@ class DaysController < ApplicationController
     params.require(:day).permit(:city, :description, :restaurant_info, :activity_info, :extra_info, :order)
   end
 
-  def pluralize
-    @itinerary = Itinerary.find(params[:itinerary_id])
-    @itinerary.days = @days
-    @count = @days.count
-    if @count == 1
-      @day_text = "DAY"
-    else
-      @day_text = "DAYS"
-    end
-  end
+  # def pluralize
+    # @itinerary = Itinerary.find(params[:itinerary_id])
+    # @itinerary.days = @days
+    # @count = @days.count
+    # if @count == 1
+      # @day_text = "DAY"
+    # else
+      # @day_text = "DAYS"
+    # end
+  # end
 
 end
